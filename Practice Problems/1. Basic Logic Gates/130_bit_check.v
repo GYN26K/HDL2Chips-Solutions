0b1ab2b6a0d603250624b0cee05d78d5ca11ab4c
Yashmith Gali <yashmithgali26@gmail.com>
2026-05-10 12:22:34 +0530

    Verilog Code

--- Practice Problems/1. Basic Logic Gates/130_bit_check.v
+++ Practice Problems/1. Basic Logic Gates/130_bit_check.v
@@ -3,12 +3,10 @@ module bit_density_threshold(
     input [3:0] threshold,
     output pass
 );
-    // Write your code here
     
      wire [3:0] count;
 
-    assign count = data[0] + data[1] + data[2] + data[3] +
-                   data[4] + data[5] + data[6] + data[7];
+    assign count = data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7];
 
     assign pass = (count >= threshold);
     
